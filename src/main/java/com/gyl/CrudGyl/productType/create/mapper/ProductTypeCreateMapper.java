967c22f7624fd5022c7f06a09874fb43f9914486
Mateo Valentin Serrano Godoy <mateovalentinserranogodoy@gmail.com>
2026-04-27 17:46:22 -0300

    chore: Agrego la etiqueta Component al mapper

--- src/main/java/com/gyl/CrudGyl/productType/create/mapper/ProductTypeCreateMapper.java
+++ src/main/java/com/gyl/CrudGyl/productType/create/mapper/ProductTypeCreateMapper.java
@@ -3,7 +3,9 @@ package com.gyl.CrudGyl.productType.create.mapper;
 import com.gyl.CrudGyl.productType.create.dto.ProductTypeCreateDto;
 import com.gyl.CrudGyl.productType.create.dto.ProductTypeCreateResponseDto;
 import com.gyl.CrudGyl.productType.entity.ProductType;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductTypeCreateMapper {
     ProductType fromDto(ProductTypeCreateDto dto) {
         return new ProductType(dto.name(), dto.description());
