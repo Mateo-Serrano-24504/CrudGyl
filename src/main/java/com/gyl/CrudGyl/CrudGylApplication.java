60c390bc5fa2561e9eb5c09a7b4e56f258be7de5
Mateo Valentin Serrano Godoy <mateovalentinserranogodoy@gmail.com>
2026-04-28 09:58:32 -0300

    chore: Elimino import incorrecto del main

--- src/main/java/com/gyl/CrudGyl/CrudGylApplication.java
+++ src/main/java/com/gyl/CrudGyl/CrudGylApplication.java
@@ -1,6 +1,5 @@
 package com.gyl.CrudGyl;
 
-import com.gyl.CrudGyl.dto.ProductoRequestDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
