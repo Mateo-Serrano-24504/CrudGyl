c41a6eac87fbfd9ae5feac6a1a256e19b6e4f0cb
Mateo Valentin Serrano Godoy <mateovalentinserranogodoy@gmail.com>
2026-04-28 09:26:02 -0300

    chore: Agrego un exception handler temporal

---
+++ src/main/java/com/gyl/CrudGyl/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.gyl.CrudGyl.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleNotFound(Exception ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
