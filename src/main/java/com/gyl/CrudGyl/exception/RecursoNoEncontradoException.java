6e2c01165d07b9afaa0dee893b4d67c809d43b46
Mateo Valentin Serrano Godoy <mateovalentinserranogodoy@gmail.com>
2026-04-28 14:04:53 -0300

    chore: Se eliminan archivos sin usar

--- src/main/java/com/gyl/CrudGyl/exception/RecursoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.gyl.CrudGyl.exception;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    public RecursoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
