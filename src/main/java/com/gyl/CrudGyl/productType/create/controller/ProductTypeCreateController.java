2e65b844c4d47a9b73f9235ec13ad99dd7d5e7f1
Mateo Valentin Serrano Godoy <mateovalentinserranogodoy@gmail.com>
2026-04-28 09:28:02 -0300

    feat: Agrego un controlador básico para crear ProductType

---
+++ src/main/java/com/gyl/CrudGyl/productType/create/controller/ProductTypeCreateController.java
@@ -0,0 +1,25 @@
+package com.gyl.CrudGyl.productType.create.controller;
+
+import com.gyl.CrudGyl.productType.create.dto.ProductTypeCreateDto;
+import com.gyl.CrudGyl.productType.create.dto.ProductTypeCreateResponseDto;
+import com.gyl.CrudGyl.productType.create.service.ProductTypeCreateService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/products/types")
+public class ProductTypeCreateController {
+    private final ProductTypeCreateService service;
+    public ProductTypeCreateController(ProductTypeCreateService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductTypeCreateResponseDto create(@Valid @RequestBody ProductTypeCreateDto dto) {
+        return this.service.create(dto);
+    }
+}
